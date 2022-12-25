5d5e52d0242734d44d2733691ee4bf42c3d4bf76
NgTrNamKhanh <109201894+NgTrNamKhanh@users.noreply.github.com>
2022-12-25 19:30:19 +0700

    Add existing project files to Git

---
+++ main.js
@@ -0,0 +1,38 @@
+var express = require('express');
+const sessions = require('express-session');
+var app = express()
+
+app.set('view engine','hbs')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
+
+const oneDay = 1000*60*60*24;
+app.use(sessions({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized:false,
+    cookie: { maxAge: oneDay },
+    resave: false
+}));
+var clientService = require('./clientService')
+app.use("/client",clientService)
+
+var productService = require('./productService')
+app.use("/product",productService)
+
+
+app.get('/',(req,res)=>{
+    if(req.session){
+        session=req.session;
+        let userName = session.userid
+        let userRole = session.userRole
+        // console.log(userName)
+        res.render('home',{'user':userName,'role':userRole})
+    }else{
+        res.render('home')
+    }
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, (req,res)=>{
+    console.log("Server is running")
+})
---
+++ productDatabaseHandler.js
@@ -0,0 +1,41 @@
+const {MongoClient,ObjectId} = require('mongodb')
+const DATABASE_URL="mongodb+srv://ElAlmas:namkhanh2510@cluster0.kk1lwoe.mongodb.net/test"
+const DATABASE_NAME='ElAlmas'
+
+async function insertNewProduct(newProduct) {
+    let db = await getDB()
+    let id = await db.collection("Products").insertOne(newProduct)
+    return id
+}
+async function getDB() {
+    let products = await MongoClient.connect(DATABASE_URL)
+    let db = products.db("CloudComputing")
+    return db
+}
+async function getAllProduct() {
+    let db = await getDB()
+    let results = await db.collection("Products").find().toArray()
+    return results
+}   
+async function updateProduct(id, productName, price, picture) {
+    let db = await getDB()
+    await db.collection("Products").updateOne({ _id: ObjectId(id)},
+        { $set: { "productName": productName, "price": price, "picture": picture } })
+}
+async function findProductById(id) {
+    let db = await getDB()
+    const productToEdit = await db.collection('Products').findOne({ _id: ObjectId(id) })
+    return productToEdit
+}
+async function deleteProductById(id) {
+    let db = await getDB()
+    await db.collection("Products").deleteOne({ _id: ObjectId(id) })
+}
+async function findProductByName(nameSearch){
+    let db = await getDB()
+    const result = await db.collection("Products").find({productName: new RegExp(nameSearch, 'i')}).toArray()
+    console.log(nameSearch)
+    console.log(result)
+    return result
+}
+module.exports = { insertNewProduct, getAllProduct, updateProduct, findProductById, deleteProductById, findProductByName}
